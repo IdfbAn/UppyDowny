ed45b7586011e2eeb152eff9b43e8747a18a6489
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-21 22:44:59 +0400

    Add project files.

---
+++ UppyDowny/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UppyDowny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Don't mercilessly vaporize text (ask users to save)
+            editor.Text = "";
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog {
+                FileName = "",
+                DefaultExt = ".ud",
+                Filter = "UpDown files|*.ud"
+            };
+
+            bool? result = openDialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    using StreamReader reader = new(openDialog.FileName);
+                    string text = reader.ReadToEnd();
+                    editor.Text = text;
+                }
+                catch (IOException exception)
+                {
+                    Console.WriteLine($"The file could not be read: {exception.Message}");
+                }
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var saveDialog = new SaveFileDialog {
+                FileName = "",
+                DefaultExt = ".ud",
+                Filter = "UpDown files|*.ud"
+            };
+
+            bool? result = saveDialog.ShowDialog();
+
+            if (result == true)
+            {
+                File.WriteAllText(saveDialog.FileName, editor.Text);
+            }
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            // This is the big one, so it's going in its own function
+            UpDown.Interpret(editor.Text);
+        }
+    }
+}
---
+++ UppyDowny/UpDown.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UppyDowny
+{
+    /// <summary>
+    /// The official implementation of UpDown, in compliance with https://esolangs.org/wiki/UpDown.
+    /// </summary>
+    /// <remarks>
+    /// NONE OF THE FUNCTIONS ARE ACTUALLY IMPLEMENTED YET!!!
+    /// This class contains the functions that implement their equivalent commands in UpDown,
+    /// and the Interpret function to bring it all together.
+    /// </remarks>
+    public static class UpDown
+    {
+        /// <summary>
+        /// Interprets the UpDown code given.
+        /// </summary>
+        /// <param name="code">The UpDown code to be interpreted.</param>
+        public static void Interpret(string code)
+        {
+            return;
+        }
+        /// <summary>
+        /// Implements the "up" command.
+        /// </summary>
+        /// <param name="variable">The variable to increment.</param>
+        /// <param name="incrementAmount">The amount to increment the variable by.</param>
+        public static void Up(int variable, int incrementAmount)
+        {
+            return;
+        }
+        /// <summary>
+        /// Implements the "down" command.
+        /// </summary>
+        /// <param name="variable">The variable to decrement.</param>
+        /// <param name="decrementAmount">The amount to decrement the variable by.</param>
+        public static void Down(int variable, int decrementAmount)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Implements the "print" command.
+        /// </summary>
+        /// <param name="variable">The variable to print as an ASCII character.</param>
+        public static void Print(int variable)
+        {
+            return;
+        }
+    }
+}
